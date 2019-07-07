d6625884eb4640fdd397f19ac53d3aa1729dd9f3
prathyusha333 <91619prathyusha@gmail.com>
2019-07-07 17:23:34 +0530

    1.Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.21 <0.6.0;
+contract DappToken{
+    uint256 public totalSupply;
+    //constructor
+    constructor() public{
+        totalSupply = 1000000;
+    }
+    //set the tokens
+    //read the number of tokens
+}
+
+
